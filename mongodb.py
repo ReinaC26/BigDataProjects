9ddb5a1910507a54375d883e4c1f8f1f6d14ef40
Carina Chen <carina.chen66@myhunter.cuny.edu>
2026-03-19 22:37:11 -0400

    added more comments

--- mongodb.py
+++ mongodb.py
@@ -1,9 +1,9 @@
 # mongodb.py
 import sys # for handling CLI input
 import csv # for reading and parsing TSV file
-from pymongo import MongoClient
+from pymongo import MongoClient 
 
-# connect to MongoDB
+# connect to local MongoDB instance
 client = MongoClient("mongodb://localhost:27017/")
 db = client["hetionet"]
 collection = db["nodes"]
@@ -17,59 +17,81 @@ def get_name(node_id):
     used during edge loading to pre-aggregate disease documents
     (pre-store all related compounds, anatomy and gene in each disease document)
     """
-    doc = collection.find_one({"id":node_id})
+    doc = collection.find_one({"id":node_id}) #finds document by id
     return doc["name"] if doc else node_id
 
 def load_nodes():
     """Load nodes.tsv into MongoDB"""
     
+    #open tsv file containing node data
     with open("data/nodes.tsv", "r") as f:
-        reader = csv.DictReader(f, delimiter="\t")
+        reader = csv.DictReader(f, delimiter="\t") #reads tsv as dictionary rows , each row is a dictionary
         
+        #iterates through each row in file
         for row in reader: 
+            #create a document to insert into mongoDB
             doc = {
                 "id" : row["id"],
                 "name" : row["name"],
                 "kind" : row["kind"],
-                "TREATS" : [], #store name of compounds that will treat this disease
-                "PALLIATES" : [],
-                "ASSOCIATES" :[], #store name of genes associated with this disease
-                "LOCALIZES" : [] #store anatomy locations this disease affects
+                #initializes relationship fields as empty lists (only for disease)
+                "TREATS" : [], #store name of compounds that treats this disease
+                "PALLIATES" : [], #compounds that palliates this disease
+                "ASSOCIATES" :[], # genes associated with this disease
+                "LOCALIZES" : [] # anatomy locations of this disease 
             }
-            collection.insert_one(doc)
+            collection.insert_one(doc) #insert document into mongoDB
     print("Nodes loaded")
     
 def load_edges():
+    """ 
+    loads edges.tsv and transforms raw edge data into an aggregated format
+    stored directly inside mongoDB doc
+    
+    RAW DATA FORMAT (edges.tsv):
+        source | metaedge | target
+        
+    STRATEGY:
+        instead of storing each edge as a separate record, we pre-aggregate relationships into its corresponding doc
+        - longer load time: process each edge types we need and update disease doc 
+        - shorter query time: perform only one lookup
+    """
+    
     with open("data/edges.tsv", "r") as f:
         reader = csv.DictReader(f, delimiter="\t")
+        
+        # Add each metaedge (relationship)
         for row in reader: 
-            source = row["source"]
-            metaedge = row["metaedge"]
-            target = row["target"]
+            source = row["source"] # ID of source node
+            metaedge = row["metaedge"] #type of relationship ("CtD", "DaG", etc)
+            target = row["target"] # ID of target node 
             
+            # CtD: Compound treats Disease
             if metaedge == "CtD" :
-                # Compound treats Disease: store compound name in disease doc , under "TREATS"
-                c_name = get_name(source)
+                c_name = get_name(source) #get compound name from ID
+                # find disease doc and add compound to TREATS list in disease doc
                 collection.update_one({"id": target}, {"$push": {"TREATS": c_name}})
             
+            # Compound palliates Disease: store compound name in disease doc , under "PALLIATES"
             elif metaedge == "CpD" :
-                # Compound palliates Disease: store compound name in disease doc , under "PALLIATES"
                 c_name = get_name(source)
                 collection.update_one({"id": target}, {"$push": {"PALLIATES": c_name}})
             
+            # Disease associates Gene aka gene cause disease: store gene name in disease doc , under "ASSOCIATES"
             elif metaedge == "DaG" :
-                # Disease associates Gene aka gene cause disease: store gene name in disease doc , under "ASSOCIATES"
                 g_name = get_name(target)
                 collection.update_one({"id": source}, {"$push": {"ASSOCIATES": g_name}})
             
+            # Disease localises Anatomy aka disease occurs in this location of body: store anatomy name in disease doc , under "LOCALIZES"
             elif metaedge == "DlA" :
-                # Disease localises Anatomy aka disease occurs in this location of body: store anatomy name in disease doc , under "LOCALIZES"
                 a_name = get_name(target)
                 collection.update_one({"id": source}, {"$push": {"LOCALIZES": a_name}})
     
     print("Edges loaded")
-    
+
+# query a disease and print its relationships    
 def query1(disease_id):
+    # retrieve disease document by its ID
     disease = collection.find_one({"id": disease_id})
     if disease is None:
         print("Disease not found")
